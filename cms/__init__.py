8459fd68602225447a6cda5a64398a00e5d6e099
thebjorn <bp@datakortet.no>
2016-11-26 23:44:11 +0100

    Remove all this stupidity (which isn't used, since the conf module doesn't exist anymore, and even if it did it should be imported using a relative path).

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,21 +1,2 @@
 # -*- coding: utf-8 -*-
 __version__ = '2.4.0'
-
-# patch settings
-try:
-    from django.conf import settings
-    if 'cms' in settings.INSTALLED_APPS:
-        from conf import patch_settings
-        patch_settings()
-except ImportError:  # pragma: no cover
-    """
-    This exception means that either the application is being built, or is
-    otherwise installed improperly. Both make running patch_settings
-    irrelevant.
-    """
-    pass
-
-except:
-    # django.core.exceptions.ImproperlyConfigured is raised if trying to
-    # build django-cms without a DJANGO_SETTINGS_MODULE defined
-    pass
